85ae98927689792016865617c3ab08962a69f910
Alessandra Trapani <alessandramaria.trapani@polimi.it>
2024-05-14 15:33:55 +0200

    update convert session and converter

--- src/reimer_arenkiel_lab_to_nwb/embargo2024/embargo2024_convert_session.py
+++ src/reimer_arenkiel_lab_to_nwb/embargo2024/embargo2024_convert_session.py
@@ -2,59 +2,64 @@
 
 from pathlib import Path
 from typing import Union
-import datetime
 from zoneinfo import ZoneInfo
 from natsort import natsorted
 
 from neuroconv.utils import load_dict_from_file, dict_deep_update
 
 from reimer_arenkiel_lab_to_nwb.embargo2024 import Embargo2024NWBConverter
-from roiextractors import ScanImageTiffSinglePlaneImagingExtractor
 
 
 def session_to_nwb(
-    data_dir_path: Union[str, Path], output_dir_path: Union[str, Path], session_id: str, stub_test: bool = False
+    data_dir_path: Union[str, Path], output_dir_path: Union[str, Path], session_date:str, session_id: str, subject_id:str, stub_test: bool = False
 ):
 
     data_dir_path = Path(data_dir_path)
-    folder_path = data_dir_path / Path(session_id)
-    subject_id = "test_subject"
+    folder_path = data_dir_path / Path(session_date)
 
     output_dir_path = Path(output_dir_path)
     if stub_test:
         output_dir_path = output_dir_path / "nwb_stub"
     output_dir_path.mkdir(parents=True, exist_ok=True)
 
-    nwbfile_path = output_dir_path / f"{session_id}.nwb"
+    nwbfile_path = output_dir_path / f"{subject_id}_{session_id}.nwb"
 
     source_data = dict()
     conversion_options = dict()
 
-    # Add Imaging Channel1
-    source_data.update(
-        dict(ImagingChannel1=dict(folder_path=str(folder_path), file_pattern="*.tif", channel_name="Channel 1"))
-    )
-    conversion_options.update(dict(ImagingChannel1=dict(stub_test=stub_test, photon_series_index=0)))
-    # Add Imaging Channel2 # not needed
-    # source_data.update(
-    #     dict(ImagingChannel2=dict(folder_path=str(folder_path), file_pattern="*.tif", channel_name="Channel 2"))
-    # )
-    # conversion_options.update(dict(ImagingChannel2=dict(stub_test=stub_test, photon_series_index=1)))
+    list_of_fov_boundaries = [[0,400], [450, 850], [900,1300]]
+    # Add Imaging Green Channel Plane1
+    for i,fov_boundaries in enumerate(list_of_fov_boundaries):
+        interface_name = f"ImagingGreenChannelFOV{i}"
+        source_data[interface_name] = {
+            "folder_path": str(folder_path),
+            "file_pattern": f"{subject_id}_{session_id}_*.tif",
+            "channel_name": "Channel 1",
+            "fov_boundaries": fov_boundaries
+        }
+        conversion_options[interface_name] = {
+            "stub_test": stub_test,
+            "photon_series_index": i,
+            "photon_series_type": "TwoPhotonSeries",
+        }
 
     converter = Embargo2024NWBConverter(source_data=source_data)
 
+
     # Add datetime to conversion
     metadata = converter.get_metadata()
-    datetime.datetime(year=2020, month=1, day=1, tzinfo=ZoneInfo("US/Eastern"))
-    date = datetime.datetime.today()  # TO-DO: Get this from author
-    metadata["NWBFile"]["session_start_time"] = date
     metadata["Subject"].update(subject_id=subject_id)
-    metadata["NWBFile"].update(session_id=session_id)
 
     # Update default metadata with the editable in the corresponding yaml file
     editable_metadata_path = Path(__file__).parent / "embargo2024_metadata.yaml"
     editable_metadata = load_dict_from_file(editable_metadata_path)
     metadata = dict_deep_update(metadata, editable_metadata)
+
+    # Add ophys metadata
+    ophys_metadata_path = Path(__file__).parent / "metadata" / "embargo2024_ophys_metadata.yaml"
+    ophys_metadata = load_dict_from_file(ophys_metadata_path)
+    metadata.pop("Ophys", None)
+    metadata = dict_deep_update(metadata, ophys_metadata)
 
     # Run conversion
     converter.run_conversion(
@@ -68,11 +73,15 @@ if __name__ == "__main__":
     data_dir_path = root_path / "Reimer-Arenkiel-CN-data-share"
     output_dir_path = root_path / "Reimer-Arenkiel-conversion_nwb/"
     stub_test = True
-    session_id = "2022-07-21_12-09-48"
+    session_date = "2022-07-21_12-09-48"
+    session_id = "22"
+    subject_id = "134"
 
     session_to_nwb(
         data_dir_path=data_dir_path,
         output_dir_path=output_dir_path,
+        session_date = session_date,
         session_id=session_id,
+        subject_id=subject_id,
         stub_test=stub_test,
     )
--- src/reimer_arenkiel_lab_to_nwb/embargo2024/embargo2024nwbconverter.py
+++ src/reimer_arenkiel_lab_to_nwb/embargo2024/embargo2024nwbconverter.py
@@ -1,11 +1,12 @@
 """Primary NWBConverter class for this dataset."""
 from neuroconv import NWBConverter
-from neuroconv.datainterfaces import ScanImageMultiFileImagingInterface
+from .interfaces.embargo2024_imaging_interface import Embargo2024ImagingInterface
 
 class Embargo2024NWBConverter(NWBConverter):
     """Primary conversion class for my extracellular electrophysiology dataset."""
 
     data_interface_classes = dict(
-        ImagingChannel1=ScanImageMultiFileImagingInterface,
-        ImagingChannel2=ScanImageMultiFileImagingInterface,
+        ImagingGreenChannelFOV0=Embargo2024ImagingInterface,
+        ImagingGreenChannelFOV1=Embargo2024ImagingInterface,
+        ImagingGreenChannelFOV2=Embargo2024ImagingInterface,
     )
